6ad2aa99c2e60738bb1b7f8842fd9ecb6678f2e5
Gasper Katrasnik <gkatrasnik@gmail.com>
2021-05-23 15:03:59 +0200

    Display data in DOM

--- main.js
+++ main.js
@@ -15,7 +15,6 @@ function filterData(data) {
     weather: data.weather[0].main,
     description: data.weather[0].description,
     temp: data.main.temp,
-    feelLike: data.main.feels_like,
     wind: data.wind.speed,
   };
   return filteredData;
@@ -23,6 +22,11 @@ function filterData(data) {
 
 //display data in DOM
 function displayData(data) {
+  cityName.textContent = data.city;
+  weather.textContent = data.weather;
+  description.textContent = data.description;
+  temperature.textContent = data.temp;
+  wind.textContent = data.wind;
   console.log(data);
 }
 
@@ -35,8 +39,16 @@ function readInput() {
 
 //start program
 
+//define DOM elements
 const form = document.getElementById("form");
+const card = document.getElementById("card");
+const cityName = document.getElementById("cityName");
+const weather = document.getElementById("weather");
+const description = document.getElementById("description");
+const temperature = document.getElementById("temp");
+const wind = document.getElementById("wind");
 
+//listen for user input submit
 form.addEventListener(`submit`, (e) => {
   let currentCity = readInput();
   e.preventDefault();
